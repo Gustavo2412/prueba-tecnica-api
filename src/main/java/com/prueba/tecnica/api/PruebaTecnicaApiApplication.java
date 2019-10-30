42c1c8610f641cdc2630c8632ee4b185ce247e79
Gustavo Paredes <gustavo.paredes@qacg.com>
2019-10-30 10:20:42 -0600

    Commit creacion de proyecto spring boots

---
+++ src/main/java/com/prueba/tecnica/api/PruebaTecnicaApiApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.tecnica.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/tecnica/api/PruebaTecnicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.tecnica.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
